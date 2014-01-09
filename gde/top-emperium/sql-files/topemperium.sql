45ddf70388d99decae1a103e8b7cd15de8bea481
SoulBlaker <romulo_c4aos@hotmail.com>
2014-01-08 22:09:33 -0200

    Atualização nas tabelas do banco de dados do topemperium.

--- gde/top-emperium/sql-files/topemperium.sql
+++ gde/top-emperium/sql-files/topemperium.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IF NOT EXISTS `topemperium_players` (
-  `char_id` int(11) unsigned NOT NULL,
-  `break` int(11) unsgined NOT NULL default '0',
-  `status` int(1) NOT NULL default '0',
+  `char_id` INT(11) NOT NULL,
+  `break` INT(11) NOT NULL DEFAULT '0',
+  `status` INT(1) NOT NULL DEFAULT '0',
   PRIMARY KEY  (`char_id`)
 ) ENGINE=MyISAM;
-
+ 
 CREATE TABLE IF NOT EXISTS `topemperium_playerscastles` (
-  `char_id` int(11) unsigned NOT NULL,
-  `castle` varchar(24) NOT NULL default '',
-  `break` int(11) unsgined NOT NULL default '0',
-  `status` int(1) NOT NULL default '0',
+  `char_id` INT(11) NOT NULL,
+  `castle` VARCHAR(24) NOT NULL DEFAULT '',
+  `break` INT(11) NOT NULL DEFAULT '0',
+  `status` INT(1) NOT NULL DEFAULT '0',
   PRIMARY KEY  (`char_id`)
 ) ENGINE=MyISAM;
-
+ 
 CREATE TABLE IF NOT EXISTS `topemperium_guilds` (
-  `guild_id` int(11) unsigned NOT NULL,
-  `break` int(11) unsgined NOT NULL default '0',
-  `status` int(1) NOT NULL default '0',
-  PRIMARY KEY  (`char_id`)
+  `guild_id` INT(11) NOT NULL,
+  `break` INT(11) NOT NULL DEFAULT '0',
+  `status` INT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY  (`guild_id`)
 ) ENGINE=MyISAM;
-
+ 
 CREATE TABLE IF NOT EXISTS `topemperium_guildscastles` (
-  `guild_id` int(11) unsigned NOT NULL,
-  `castle` varchar(24) NOT NULL default '',
-  `break` int(11) unsgined NOT NULL default '0',
-  `status` int(1) NOT NULL default '0',
-  PRIMARY KEY  (`char_id`)
+  `guild_id` INT(11) NOT NULL,
+  `castle` VARCHAR(24) NOT NULL DEFAULT '',
+  `break` INT(11) NOT NULL DEFAULT '0',
+  `status` INT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY  (`guild_id`)
 ) ENGINE=MyISAM;
